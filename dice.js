84480e68bc8f3e84e11c7b4fbafde66e4b3987bf
tabitha-codes <tabithacodes@gmail.com>
2026-06-25 21:51:49 -0500

    Add rollDice function and connect to UI

--- dice.js
+++ dice.js
@@ -1,17 +1,11 @@
 // Notes Ref: https://www.w3schools.com/JS/js_random.asp
-// function rollDice (sides) {
-//   Math.floor(Math.random() * sides + 1);  
-//     return(sides)
-// }
-
-// console.log()
-
 // Dice Function
-function rollDice (sides) {
+function rollDice (sides, resultId) {
     let result = Math.floor(Math.random() * sides + 1);
+    document.getElementById(resultId).textContent = result; 
     return result;
 }
 
-console.log(rollDice(6))
-console.log(rollDice(10))
-console.log(rollDice(20))
+// console.log(rollDice(6))
+// console.log(rollDice(10))
+// console.log(rollDice(20))
